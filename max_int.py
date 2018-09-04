ee65225c13936356dd9f987706ba58c0c8e9b2a0
Hildur Kolfinna <hildurs18@ru.is>
2018-09-04 15:18:39 +0000

    code added to correct file

---
+++ max_int.py
@@ -0,0 +1,15 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int = num_int
+print("The maximum is", max_int)    # Do not change this line
+
+
+#Biðja um tölu
+#Biðja um næstu tölu og bera hana saman við fyrstu töluna
+#Finna stærri töluna
+#Biðja um næstu tölu og bera saman við stærri töluna frá því áðan
+#o.s.frv yoyoo
